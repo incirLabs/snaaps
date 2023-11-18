54b0d08def57893de6831453a967f6837b8a785f
Uğur Eren <contact@ugureren.net>
2023-11-18 03:05:16 +0300

    Use header in page layout

--- packages/site/src/components/PageLayout/PageLayout.tsx
+++ packages/site/src/components/PageLayout/PageLayout.tsx
@@ -1,12 +1,15 @@
 import {Outlet} from 'react-router-dom';
 import {PageContainer} from './PageContainer';
+import {Header} from '../Header/Header';
 
 import './styles.scss';
 
 export const PageLayout: React.FC<{children?: React.ReactNode}> = ({children}) => {
   return (
     <div className="c-page-layout">
-      <PageContainer area="top">HEADER</PageContainer>
+      <PageContainer area="top">
+        <Header />
+      </PageContainer>
 
       {children}
 
