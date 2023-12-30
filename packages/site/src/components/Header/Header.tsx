452895add930edf7bbfbedc2cccc80e397727e73
Uğur Eren <contact@ugureren.net>
2023-12-30 21:33:00 +0300

    Change logo

--- packages/site/src/components/Header/Header.tsx
+++ packages/site/src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import {Button} from '../Button/Button';
 
+import {LogoSquare} from '../../assets/LogoSquare';
+
 import './styles.scss';
 
 export const Header: React.FC = () => {
@@ -7,7 +9,10 @@ export const Header: React.FC = () => {
 
   return (
     <div className="c-header">
-      <h1 className="c-header_logo">SnAAps 😸</h1>
+      <h1 className="c-header_logo">
+        <LogoSquare width={32} height={32} className="me-3" />
+        snAAps
+      </h1>
 
       {connected ? (
         <Button theme="chip" className="c-header_connected-button">
