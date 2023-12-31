04f606cf0cd770853877ee662d029aa17a66c1ee
Uğur Eren <contact@ugureren.net>
2023-12-31 14:35:50 +0300

    Beta warning banner

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -1,7 +1,25 @@
+import {Fragment} from 'react';
 import Router from './routes/Router';
 
 const App: React.FC = () => {
-  return <Router />;
+  return (
+    <div className="app">
+      <div className="app_warning-banner">
+        <div className="app_warning-banner_content">
+          {Array.from({length: 20}).map((_, index) => (
+            <Fragment key={index.toString()}>
+              <span>Currently In Beta</span>
+              <span>-</span>
+              <span>Take Your Risk</span>
+              <span>-</span>
+            </Fragment>
+          ))}
+        </div>
+      </div>
+
+      <Router />
+    </div>
+  );
 };
 
 export default App;
