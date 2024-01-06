c85c7dbb689e944d8216e08ed8c8e03c13592031
Uğur Eren <contact@ugureren.net>
2024-01-06 15:52:59 +0300

    Remove setup logic

--- packages/site/src/routes/Setup/Setup.tsx
+++ packages/site/src/routes/Setup/Setup.tsx
@@ -1,69 +1,13 @@
-import {useEffect} from 'react';
-import {Env} from 'common';
 import cx from 'classnames';
-import {Wallet} from 'ethers';
-import {useNavigate} from 'react-router-dom';
-import {KeyringSnapRpcClient} from '@metamask/keyring-api';
-import {useAccount, useContractRead, useContractWrite} from '@incirlabs/react-ethooks';
 import {Button, Surface, PageContainer} from '../../components';
-import {useSnaapAddress, useSimpleAccountFactory} from '../../hooks';
-import {Paths} from '../Paths';
 
 import './styles.scss';
 
 const Setup: React.FC = () => {
-  const {address} = useAccount();
-  const navigate = useNavigate();
-  const snaapAddress = useSnaapAddress();
-
-  useEffect(() => {
-    if (!address) navigate('/', {replace: true});
-    if (snaapAddress) navigate(Paths.MySnaap.Root, {replace: true});
-  }, [address, snaapAddress, navigate]);
-
-  const AccountFactory = useSimpleAccountFactory();
-  const createAccount = useContractWrite(AccountFactory, 'createAccount');
-  const getAccountAddress = useContractRead(AccountFactory, 'getAddress');
-
-  const onDeployClick = async () => {
-    const wallet = Wallet.createRandom();
-
-    const tx = await createAccount([wallet.address, 0]);
-    if (!tx.status) {
-      console.error('tx error', tx.error);
-      return;
-    }
-
-    const receipt = await tx.data.wait();
-
-    const aaAddressRes = await getAccountAddress([wallet.address, 0]);
-
-    if (!aaAddressRes.status) {
-      console.error("Couldn't get AA address", aaAddressRes.error);
-      return;
-    }
-
-    const aaAddress = aaAddressRes.data;
-
-    try {
-      const client = new KeyringSnapRpcClient(Env.SNAP_ORIGIN, window.ethereum);
-
-      const account = await client.createAccount({
-        type: 'eip155:eip4337',
-        address: aaAddress,
-        privateKey: wallet.privateKey.replace('0x', ''),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <PageContainer area="center" className={cx('p-setup')}>
       <Surface className="p-setup_content">
-        <Button theme="chip" onClick={onDeployClick}>
-          + Create and deploy a new AA signer
-        </Button>
+        <Button theme="chip">+ Create and deploy a new AA signer</Button>
       </Surface>
     </PageContainer>
   );
