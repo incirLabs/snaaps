0b3a5a0da0a2b7819ee9de96565060ea425b645a
Uğur Eren <contact@ugureren.net>
2024-01-15 13:20:52 +0300

    Show no wallets warning in my snaaps

--- packages/site/src/routes/MySnaaps/MySnaaps.tsx
+++ packages/site/src/routes/MySnaaps/MySnaaps.tsx
@@ -30,20 +30,29 @@ const MySnaaps: React.FC = () => {
   return (
     <PageContainer className={cx('p-my-snaaps')}>
       <PageContainer.Card className="p-my-snaaps_content" title="Select an Account">
-        <div className="p-my-snaaps_wallets">
-          {accounts.map((account) => (
-            <AccountCard
-              key={account.id}
-              text={account.address}
-              walletAddress={account.address}
-              right={
-                <Button theme="chip" as={Link} to={Paths.MySnaap(account.address).MySnaap}>
-                  Configure
-                </Button>
-              }
-            />
-          ))}
-        </div>
+        {accounts.length > 0 ? (
+          <div className="p-my-snaaps_wallets">
+            {accounts.map((account) => (
+              <AccountCard
+                key={account.id}
+                text={account.address}
+                walletAddress={account.address}
+                right={
+                  <Button theme="chip" as={Link} to={Paths.MySnaap(account.address).MySnaap}>
+                    Configure
+                  </Button>
+                }
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="d-flex f-dir-col align-center">
+            <h3>You don't have any wallets</h3>
+            <Button theme="chip" color="dark" as={Link} to={Paths.Landing.CreateNew}>
+              Get an AA Wallet
+            </Button>
+          </div>
+        )}
 
         {loading ? <ActivityIndicator size="normal" className="w-100" /> : null}
       </PageContainer.Card>
